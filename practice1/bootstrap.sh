8d5242d23dfbc0e2a74a40c7695da3bd6cb8db0a
seungjuoh <sjoh0704@daum.net>
2021-08-21 02:23:22 +0900

    add ansible setting

--- practice1/bootstrap.sh
+++ practice1/bootstrap.sh
@@ -1,3 +1,12 @@
 #! /usr/bin/env bash
+
+# ansible 설치 
 yum install epel-release -y
 yum install ansible -y
+
+
+# 환경 설정 초기파일 구성 for vagrant only (사용자가 vagrant이므로)
+mkdir -p /home/vagrant/.vim/autoload /home/vagrant/.vim/bundle
+touch /home/vagrant/.vimrc
+touch /home/vagrant/.bashrc
+ 

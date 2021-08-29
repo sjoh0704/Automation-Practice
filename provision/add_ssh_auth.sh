a875f2f2e932782bbad1099aed3a97d55f6fe038
seungjuoh <sjoh0704@daum.net>
2021-08-29 17:26:35 +0900

    add ubuntu

--- provision/add_ssh_auth.sh
+++ provision/add_ssh_auth.sh
@@ -3,5 +3,8 @@
 # ssh key 생성
 sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node01
 sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node02
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node03
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@node04
+
 
 # vagrant 유저로 ssh를 통해서 접속한다. 
